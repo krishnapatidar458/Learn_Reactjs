3cc858f1b2494a9e0c0368ca56c9bbfdf240ecea
Krishna Patidar <92305156+krishnapatidar458@users.noreply.github.com>
2024-09-12 17:38:15 +0000

    Day-2

--- Day-1/palindromeNumber.java
+++ Day-1/palindromeNumber.java
@@ -2,7 +2,7 @@ class palindromeNumber{
     public static void main(String args[]){
         int n = 153;
         int temp = n,result=0;
-        while(n>0){
+        while(n>0){ 
             result = result*10+ n%10;
             n/=10;
         }
