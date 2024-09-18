09d94c5fc1532460b703caa8d99d23e9bd98aa36
Krishna Patidar <92305156+krishnapatidar458@users.noreply.github.com>
2024-09-18 13:33:43 +0000

    completed all patterns

--- Day-6/Pattern.java
+++ Day-6/Pattern.java
@@ -1,3 +1,5 @@
+import java.lang.Math;
+
 class Pattern{
     public static void main(String[] args){
         // oneTo5(5);
@@ -6,7 +8,67 @@ class Pattern{
         // zeroOne(5);
         // Fifty50(5);
         // OneTon(5);
-        EmptyDimond(5);
+        // EmptyDimond(5);
+        // Butterfly(5);
+        // EmptyBoc(5);
+        spiralBox(5);
+
+    }
+
+    static void spiralBox(int n){
+        for(int i=0;i<2*n-1;i++){
+            for(int j=0;j<2*n-1;j++){
+                int top = i;
+                int bottom = j;
+                int left =(2*n-2)-i;
+                int right = (2*n-2)-j;
+
+            System.out.print(n - Math.min(Math.min(top,bottom),Math.min(left,right)) + " ");
+            }
+            System.out.println();
+        }
+    }    
+
+    static void EmptyBoc(int n){
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                if(i==0 || i==n-1 || j==0 || j==n-1){
+                    System.out.print(0);
+                }else{
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+    }
+        
+
+    static void Butterfly(int n){
+        for(int i=1;i<=n-1;i++){
+            for(int j=1;j<=i;j++){
+                System.out.print("*");
+            }
+            for(int s=1;s<=2*(n-i);s++){
+                System.out.print(" ");
+            }
+            for(int j=1;j<=i;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+        for(int i=1;i<=n;i++){
+            for(int j=n;j>=i;j--){
+                System.out.print("*");
+            }
+            for(int s=2*(i-1);s>=1;s--){
+                System.out.print(" ");
+            }
+            for(int j=n;j>=i;j--){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
     }
 
     static void EmptyDimond(int n){
